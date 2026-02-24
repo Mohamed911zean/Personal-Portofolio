92f1086e1fae47c36dd12367c0dbc3f19ce48b34
Moahmed Zean <elkonmohamed911@gmail.com>
2026-02-24 22:29:13 +0200

    vercel depoyment

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import './App.css'
 import NavBar from './NavBar.jsx'
-import { BrowserRouter , Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Footer.jsx'
 import Home from './Home.jsx'
 import Projects from './Projects.jsx'
+
 const App = () => {
   return (
-    <BrowserRouter basename='/Personal-Portofolio'>
-
-    <NavBar/>
-
-    <Routes>
-      
-
-<Route path='/' element={<Home/>} />
-<Route path='/projects' element={<Projects/>}/>
-    </Routes>
-
-    <Footer/>
-
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
